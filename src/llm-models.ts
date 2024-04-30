23ac9b1c99b625e7ff1741814dfd9f49d967608a
Riceball LEE <snowyu.lee@gmail.com>
2024-04-30 08:13:27 +0800

    refactor: LlmModelsFunc $download and add $getUrl methods

--- src/llm-models.ts
+++ src/llm-models.ts
@@ -19,15 +19,19 @@ import {
 import { KVSqliteResFunc, KVSqliteResFuncParams,  } from "@isdk/ai-tool-sqlite";
 import { DownloadName, download } from '@isdk/ai-tool-download'
 
-import { AIModelSettings } from './llm-settings';
+import { AIModelFileSettings, AIModelQuantType, AIModelSettings } from './llm-settings';
 import { getHFUrl } from './utils';
 import { LLMProviderName, llm } from './llm-provider';
 import { LLMArguments } from './llm-options';
 
-const MODELS_DB_NAME = '.modelsdb'
+export const MODELS_DB_NAME = '.modelsdb'
 // const eventBus = event.runSync()
 
 interface LlmModelsFuncParams extends AIModelSettings, KVSqliteResFuncParams {
+}
+
+function findModelFileByQuant(files: AIModelFileSettings[], quant: AIModelQuantType) {
+  return files.find(file => file.quant === quant)
 }
 
 export class LlmModelsFunc extends KVSqliteResFunc<LlmModelsFuncParams> {
@@ -41,11 +45,19 @@ export class LlmModelsFunc extends KVSqliteResFunc<LlmModelsFuncParams> {
 
   constructor(name: string|Function|FuncItem, options: FuncItem|any = {}) {
     const rootDir = options.rootDir
-    if (!rootDir) {throw new CommonError('rootDir is required', 'LlmModels', ErrorCode.InvalidArgument)}
+    if (!rootDir) {throw new CommonError('rootDir is required', 'LlmModelsFunc', ErrorCode.InvalidArgument)}
     download.rootDir = rootDir
-    options.dbPath = path.join(rootDir, MODELS_DB_NAME)
+    if (!options.dbPath) {options.dbPath = path.join(rootDir, MODELS_DB_NAME)}
 
     super(name, options)
+  }
+
+  initDB() {
+    const filename = path.resolve(this.rootDir!, '..', 'models', 'gguf-models.json');
+    if (fs.existsSync(filename)) {
+      const configs = JSON.parse(fs.readFileSync(filename, 'utf8'));
+      this.db.bulkDocs(configs);
+    }
   }
 
   verifyDownloaded(model: AIModelSettings) {
@@ -53,7 +65,7 @@ export class LlmModelsFunc extends KVSqliteResFunc<LlmModelsFuncParams> {
       const downloaded = model.downloaded
       if (downloaded) {
         if (model.location) {
-          const location = model.location ?? path.join(this.rootDir!, model._id)
+          const location = model.location ?? path.join(this.rootDir!, model._id!)
           if (fs.existsSync(location)) {
             if (model.file_size) {
               const stat = fs.statSync(location)
@@ -77,31 +89,44 @@ export class LlmModelsFunc extends KVSqliteResFunc<LlmModelsFuncParams> {
     return params?.stream
   }
 
-  getPath(model: string|AIModelSettings) {
-    if (typeof model === 'string') {
-      model = this.db.get(model)
+  getFileInfo(id: string, quant: AIModelQuantType) {
+    const model = this.db.get(id)
+    if (!model) {
+      throw new NotFoundError(id, this.name + '.getFileInfo')
     }
-    let result = model.location ?? model._id
+    const fileInfo = findModelFileByQuant(model.files, quant) as AIModelFileSettings
+    if (!fileInfo) {
+      throw new NotFoundError(`${id}.${quant}`, this.name + '.getFileInfo')
+    }
+    fileInfo.hf_repo = model.hf_repo
+    return fileInfo
+  }
+
+  getPath(fileInfo: string|AIModelSettings, quant: AIModelQuantType) {
+    if (typeof fileInfo === 'string') {
+      fileInfo = this.getFileInfo(fileInfo, quant)
+    }
+    const result = fileInfo.location ?? fileInfo.file_name
     return result
   }
 
-  getUrl(model: string|AIModelSettings) {
-    if (typeof model === 'string') {
-      model = this.db.get(model)
+  getUrl(fileInfo: string|AIModelFileSettings, quant: AIModelQuantType) {
+    if (typeof fileInfo === 'string') {
+      fileInfo = this.getFileInfo(fileInfo, quant)
     }
-    let result = model.url ?? this.getUrlFromHf(model)
+    const result = fileInfo.url ?? this.getUrlFromHf(fileInfo)
     return result
   }
 
-  getUrlFromHf(model: AIModelSettings, hubUrl?: string) {
-    if (model.hf_repo && model.hf_path) {
+  getUrlFromHf(fileInfo: AIModelFileSettings, hubUrl?: string) {
+    if (fileInfo.hf_repo && fileInfo.hf_path) {
       if (this.usingMirror && !hubUrl) {
         hubUrl = 'https://hf-mirror.com'
       }
 
       return getHFUrl({
-        repo: model.hf_repo,
-        path: model.hf_path,
+        repo: fileInfo.hf_repo,
+        path: fileInfo.hf_path,
         hubUrl,
       })
     }
@@ -111,39 +136,64 @@ export class LlmModelsFunc extends KVSqliteResFunc<LlmModelsFuncParams> {
     const id = model.id
     const val = model.val
     if (id && val) {
+      val._id = id
       this.verifyDownloaded(val)
     }
     return super.put(model)
   }
 
-  async $download({id}: LlmModelsFuncParams) {
+  async _getUrlByParams({id, quant}: LlmModelsFuncParams) {
     if (!id) {
-      throw new CommonError('id is required', 'LlmModels.download', ErrorCode.InvalidArgument)
+      throw new CommonError('id is required', this.name + '._getUrlByParams', ErrorCode.InvalidArgument)
     }
+    if (!quant) {
+      throw new CommonError('quant is required', this.name + '._getUrlByParams', ErrorCode.InvalidArgument)
+    }
+
+    if (typeof quant === 'string') { quant = AIModelQuantType[quant as any] }
+
+    if (AIModelQuantType[quant] === undefined) {
+      throw new CommonError('quant is invalid', this.name + '._getUrlByParams', ErrorCode.InvalidArgument)
+    }
+
     const model = this.db.get(id) as AIModelSettings
 
     if (!model) {
-      throw new NotFoundError(id, 'LlmModels.download')
+      throw new NotFoundError(id, this.name + '._getUrlByParams')
     }
 
-    if (model.downloaded) {
-      throw new AlreadyExistsError(model.id!, 'LlmModels.download')
+    const file = this.getFileInfo(id, quant)
+
+    const url = this.getUrl(file, quant)
+    const filepath = this.getPath(file, quant)
+    if (!url || !filepath) {
+      throw new CommonError('Can not get url or filepath', this.name + '._getUrlByParams', ErrorCode.InvalidArgument)
+    }
+    return {url, filepath, file, id, quant}
+  }
+
+  async $getUrl(params: LlmModelsFuncParams) {
+    const {url, filepath} = await this._getUrlByParams(params)
+    return {url, filepath}
+  }
+
+  async $download(params: LlmModelsFuncParams) {
+    let {overwrite} = params
+
+    const {url, filepath, file, id, quant} = await this._getUrlByParams(params)
+
+    if (file.downloaded) {
+      throw new AlreadyExistsError(`${id}.${quant}`, this.name + '.download')
     }
 
-    const url = this.getUrl(model)
-    const filepath = this.getPath(model)
-    const overwrite = model.overwrite ?? false
-    if (url && filepath) {
-      const result = await download.post({
-        url,
-        filepath,
-        overwrite,
-        start: true,
-      })
-      return result;
-    } else {
-      throw new CommonError('url or filepath is required', 'LlmModels.download', ErrorCode.InvalidArgument)
-    }
+    overwrite = overwrite ?? false
+    const result = await download.post({
+      url,
+      filepath,
+      overwrite,
+      start: true,
+    })
+    return result;
   }
 
   async $run(input: LLMArguments) {
